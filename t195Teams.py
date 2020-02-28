1de4693ef4811553bb2e8ece1c1b03b76cc9610c
bontempi <chrisbontempi@compuserve.com>
2020-02-28 12:57:04 -0500

    Added debugging and the Teams interface.

--- t195Teams.py
+++ t195Teams.py
@@ -5,7 +5,7 @@ import frcteam195.database as t195db
 def respond(err, res=None):
     return {
         'statusCode': '400' if err else '200',
-        'body': err.message if err else json.dumps(res),
+        'body': err if err else json.dumps(res),
         'headers': {
             'Content-Type': 'application/json',
         },
@@ -23,13 +23,14 @@ def lambda_handler(event, context):
         conn = t195db.connect()
         print("Database host is {}".format(conn.server_host))
         cursor = conn.cursor()
-        cursor.execute("SELECT * FROM Users ORDER BY LastName, FirstName")
+        cursor.execute("SELECT a.* FROM Teams a, MatchScouting b, Events c " +
+                       "WHERE a.Team = b.Team and b.EventID = c.EventID and c.CurrentEvent = 1")
         columns = [column[0] for column in cursor.description]
         results = []
         for row in cursor.fetchall():
             results.append(dict(zip(columns, row)))
         if 0 == len(results):
-            return respond('The Users table is empty!')
+            return respond('No teams records were found!!')
 
         return respond(None, results)
     else:
